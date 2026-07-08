2331b2a07933dfee801f82591ab495e21f047d57
Ittikorn Ice <ittikorntongsima@gmail.com>
2026-07-09 03:38:46 +0700

    Initial commit: add Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Profile',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: const ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('โปรไฟล์ของฉัน'),
+        backgroundColor: Colors.orange,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+
+            // รูปโปรไฟล์
+            const CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.orange,
+              child: Icon(Icons.person, size: 60, color: Colors.white),
+            ),
+
+            const SizedBox(height: 16),
+
+            // ชื่อ — TODO: เปลี่ยนเป็นชื่อของคุณ
+            const Text(
+              'ITTIKORN TONGSIMA',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 8),
+
+            // รหัสนักศึกษา — TODO: เปลี่ยนเป็นรหัสของคุณ
+            const Text(
+              'รหัสนักศึกษา: 67030261',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+
+            const SizedBox(height: 24),
+
+            // Card ข้อมูล
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    _buildInfoRow(Icons.school, 'คณะ', 'เทคโนโลยีคอมพิวเตอร์'),
+                    const Divider(),
+                    _buildInfoRow(
+                      Icons.code,
+                      'วิชาที่ชอบ',
+                      'Project + Mobile Development ครับ ',
+                    ),
+                    const Divider(),
+                    _buildInfoRow(
+                      Icons.star,
+                      'เป้าหมาย',
+                      'ผ่านโปรเจคเเละพัฒนาแอปที่สมบูรณ์ซักตัว',
+                    ),
+                    const Divider(),
+                    _buildInfoRow(
+                      Icons.star,
+                      'อาหารที่ชอบ',
+                      'ก๋วยเตี๋ยวหมูน้ำตก',
+                    ),
+                    const Divider(),
+                    _buildInfoRow(
+                      Icons.star,
+                      'สัตว์เลี้ยงที่ชอบ',
+                      'เเมงกระพรุน',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Helper Method สร้างแถวข้อมูล
+  Widget _buildInfoRow(IconData icon, String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Icon(icon, color: const Color.fromARGB(255, 246, 19, 19)),
+          const SizedBox(width: 12),
+          Text('$label: ', style: const TextStyle(fontWeight: FontWeight.bold)),
+          Expanded(child: Text(value)),
+        ],
+      ),
+    );
+  }
+}
